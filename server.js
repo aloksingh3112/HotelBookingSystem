22801b829bfbb301f1d0513508776d5e42bcceea
aloksingh3112 <aloksingh3112@gmail.com>
2019-01-17 00:58:46 +0530

    added server

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const path=require('path');
+const http=require('http');
+const app=express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(express.static(path.join(__dirname,'dist')))
+
+
+
+const port= process.env.port||'3000';
+app.set('port',port);
+app.get('*',(req,res)=>{
+ res.status('404').json({
+   error:'not found'
+ })
+
+})
+
+const server= http.createServer(app);
+
+server.listen(port,()=>{
+  console.log('connected to server'+port)
+})
+
+
 
 
