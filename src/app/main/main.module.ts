bd6aff00732b4472343e2c6841101d7292b17a42
aloksingh3112 <aloksingh3112@gmail.com>
2019-02-08 16:40:18 +0530

    added components

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,12 +1,17 @@
+import { LoginComponent } from './Login/login.component';
+import { RegisterComponent } from './Register/register.component';
+import { HomeComponent } from './Home/home.component';
 import { MainRoutingModule } from './mainrouting.module';
-import { BodyComponent } from './body/body.component';
 import { MainComponent } from './main.component';
 import {HeaderComponent} from './Header/header.component';
 import { NgModule } from '@angular/core';
+import { AboutComponent } from './About/about.component';
+
+
 
 
 @NgModule({
-  declarations : [MainComponent,HeaderComponent,BodyComponent],
+  declarations : [MainComponent,HeaderComponent,HomeComponent,RegisterComponent,LoginComponent,AboutComponent],
   imports: [MainRoutingModule],
   providers: [],
   exports: [MainComponent]
