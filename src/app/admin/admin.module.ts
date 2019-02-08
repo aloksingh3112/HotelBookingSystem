9e91dc8e3a4b60d8ceb462cb18233780e607183f
aloksingh3112 <aloksingh3112@gmail.com>
2019-02-08 16:52:36 +0530

    added admin component

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,16 @@
+import { AdminComponent } from './admin.component';
+import { NgModule } from '@angular/core';
+
+
+
+
+
+@NgModule({
+  declarations:[AdminComponent],
+  imports:[],
+  providers:[]
+
+})
+export class AdminModule{
+
+}
---
+++ src/app/admin/adminrouting.module.ts
@@ -0,0 +1,15 @@
+import { AdminComponent } from './admin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+
+
+@NgModule({
+  imports:[RouterModule],
+  exports:[]
+
+})
+export class AdminRouting{
+
+}
