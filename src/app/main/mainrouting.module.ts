a9d9fd6a6b19282ca30df8db905c77e624217869
aloksingh3112 <aloksingh3112@gmail.com>
2019-02-08 16:43:48 +0530

    added routes

--- src/app/main/mainrouting.module.ts
+++ src/app/main/mainrouting.module.ts
@@ -1,11 +1,27 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './Home/home.component';
+import { AboutComponent } from './About/about.component';
+import { LoginComponent } from './Login/login.component';
+import { RegisterComponent } from './Register/register.component';
 
 
 const routes:Routes=[
   {
-
-
+    path:'',
+    component:HomeComponent
+  },
+  {
+   path:'about',
+   component:AboutComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
   }
 ]
 
