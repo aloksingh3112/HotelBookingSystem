5bcb99f715a33a8731ff0f6caa40a4a739d49043
aloksingh3112 <aloksingh3112@gmail.com>
2019-02-08 17:20:46 +0530

    added user module

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { UserComponent } from './user.component';
+import { NgModule } from '@angular/core';
+
+
+
+
+@NgModule({
+  declarations:[UserComponent],
+  imports:[],
+  providers:[],
+
+
+})
+export class UserModule {
+ }
