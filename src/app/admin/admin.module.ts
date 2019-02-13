e9be6271f75c61c0da654914559c9317aaf6a366
aloksingh3112 <aloksingh3112@gmail.com>
2019-02-13 17:32:29 +0530

    added admin routes

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,21 +1,30 @@
+import { RoomReportComponent } from './roomreport/roomreport.component';
+import { RoomComponent } from './room/room.component';
+import { FacilityReportComponent } from './facilityreport/facilityreport.component';
+import { FacilityComponent } from './facility/facility.component';
+import { CategoryReportComponent } from './categoryreport/categoryreport.component';
+import { CategoryComponent } from './category/category.component';
+import { BookingComponent } from './Booking/booking.component';
 import { AccountComponent } from './account/account.component';
 import { CommonModule } from '@angular/common';
 import { AdminRouting } from './adminrouting.module';
 
 import { NgModule } from '@angular/core';
 
-
-
-
-
-
 @NgModule({
-  declarations:[AccountComponent],
-  imports:[AdminRouting,CommonModule],
-  providers:[],
+  declarations: [
+    AccountComponent,
+    AccountComponent,
+    BookingComponent,
+    CategoryComponent,
+    CategoryReportComponent,
+    FacilityComponent,
+    FacilityReportComponent,
+    RoomComponent,
+    RoomReportComponent,
 
-
+  ],
+  imports: [AdminRouting, CommonModule],
+  providers: []
 })
-export class AdminModule{
-
-}
+export class AdminModule {}
